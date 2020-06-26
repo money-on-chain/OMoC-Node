49dbd96bfbecdbe74038ec4f9cf8e08170e6d649
adji <andres.adjimann@coinfabrik.com>
2020-06-26 10:21:37 -0300

    fixes to support the new contracts
    Specifically:
    - We can not mint directly anymore, minting
    must be done by gobernanza.
    - getLastPubBlock is now lastPubBlock

--- servers/scripts/supporter_add_earnings.py
+++ servers/scripts/supporter_add_earnings.py
@@ -10,12 +10,7 @@ async def main():
     available_mocs = await moc_token_service.balance_of(script_settings.SCRIPT_REWARD_BAG_ACCOUNT.addr)
     print("AVAILABLE MOCS: ", available_mocs)
     if available_mocs < script_settings.REWARDS:
-        tx = await moc_token_service.mint(script_settings.SCRIPT_REWARD_BAG_ACCOUNT.addr, script_settings.REWARDS,
-                                          account=script_settings.SCRIPT_REWARD_BAG_ACCOUNT,
-                                          wait=True)
-        if is_error(tx):
-            print("ERROR IN APPROVE", tx)
-            return
+        raise Exception("WE MUST MINT BY GOBERNANZA, check contacts/scripts/mint_tokens.js!!!")
 
     tx = await moc_token_service.transfer(conf.SUPPORTERS_VESTED_ADDR,
                                           script_settings.REWARDS,
