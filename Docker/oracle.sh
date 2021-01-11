48902db737ba85f77d277720a7cae81bc3cdc327
AlejoL <49210713+AlejoLovallo@users.noreply.github.com>
2021-01-11 11:16:47 -0300

    feat(Docker): add docs

--- Docker/oracle.sh
+++ Docker/oracle.sh
@@ -8,9 +8,6 @@ function isOracleStarting {
     read answerOracle
     if [ "$answerOracle" = "y" ]; then
         STARTORACLE=true
-        #read -p "Do you want to start backend oracle service also? (y/n) " answerBackend
-        #if [ $answerBackend == 'y'];then
-        #    STARTBACKEND=true
     fi
 }
 
@@ -19,9 +16,6 @@ function startOracle {
     if [ "$STARTORACLE" = "true" ]; then
         pm2 start --name oracle servers/run_oracle.sh
     fi
-    #ASK how to run backend 
-    #if [ $STARTBACKEND];then
-    #    pm2 start --name backend monitor/backend/
 }
 
 echo "---- Starting oracle set up -----"
