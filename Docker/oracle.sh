a395fb2f108c91d5629950d73d30c589619826dd
AlejoL <49210713+AlejoLovallo@users.noreply.github.com>
2021-03-15 19:38:10 -0300

    feat(docker_refactor): Dockerfile, setAddres script

--- Docker/oracle.sh
+++ Docker/oracle.sh
@@ -1,31 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
-STARTORACLE=false
-FIRSTTIME=false
-
-function isFirstTime {
-    cd ./monitor/backend
-    hasOracleEnv=$(find ./ -name ".env" | wc -l)
-    if [ $hasOracleEnv -ne 1 ]; then
-        FIRSTTIME=true
-        touch .env
-        cp dotenv_example .env
-        cd /omoc-node/servers
-        touch .env
-        cp dotenv_example .env
-    fi
-}
-
-function main {
-        echo "---- Starting oracle -----"
-        isFirstTime
-            if [ "$FIRSTTIME" = "true" ]; then
-                echo "----- Starting oracle initial set up -----"
-                cd /omoc-node
-                python3 scripts/setAddress.py
-             fi
-        cd /omoc-node/servers
-        pm2-runtime --name oracle run_oracle.sh
-}
-
-main
+cd ./servers
+python3 -m oracle.src.main
