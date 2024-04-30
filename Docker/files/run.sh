c6fcb888db91a0feffdf5100a50d0aa4a911e6b3
Juan S. Bokser <juan.bokser@gmail.com>
2024-04-30 10:25:52 -0300

    feat: adds NOT_RUN_MPS_API_SERVER env param

---
+++ Docker/files/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "$NOT_RUN_MPS_API_SERVER" != true ] ; then
+    
+    echo ""
+    echo ""
+    echo "Start local Redis server..."
+    redis-server --save "" --appendonly no &
+    sleep 5
+
+    echo ""
+    echo ""
+    echo "Starts MoC prices source local API Rest webservice..."
+    moc_prices_source_api &
+    sleep 5
+
+fi
+
+echo ""
+echo ""
+echo "Starts Decentralized oracle node..."
+export TZ=UTC
+export CONTRACT_ROOT_FOLDER=./
+python -m oracle.src.main
